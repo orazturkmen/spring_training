c8cd1aed353c69cd6302bec45b7a475d4a66f764
oraz <hudayberdiyevoraz@gmail.com>
2024-03-24 22:07:29 +0100

    stereotype annotation practice

---
+++ spring-review-IoC/src/main/java/stereotype_annotation/config/Config.java
@@ -0,0 +1,9 @@
+package stereotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("stereotype_annotation")
+public class Config {
+}
---
+++ spring-review-IoC/src/main/java/stereotype_annotation/model/Devops.java
@@ -0,0 +1,7 @@
+package stereotype_annotation.model;
+
+public class Devops {
+    public static void getTotalHours(){
+        System.out.println();
+    }
+}
