d146f2b5b01102d9bd8b48e3b89f2fb19393dbcb
oraz <hudayberdiyevoraz@gmail.com>
2024-03-28 06:26:39 +0100

    RequestMapping annotation

--- spring-08-MVC-Conytoller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Conytoller/src/main/java/com/cydeo/controller/HomeController.java
@@ -1,7 +1,19 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home.html";
+    }
+
+
+    @RequestMapping("product")
+    public String product(){
+        return "product.html";
+    }
 }
