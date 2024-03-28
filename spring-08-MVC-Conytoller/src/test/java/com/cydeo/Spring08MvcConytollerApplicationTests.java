493c99d1c552e9b1a5834fe21465c7629772c669
oraz <hudayberdiyevoraz@gmail.com>
2024-03-28 05:53:26 +0100

    Controller created

---
+++ spring-08-MVC-Conytoller/src/test/java/com/cydeo/Spring08MvcConytollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08MvcConytollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
