08e71599eed3ee9f192a08509f752eb189fabf5a
oraz <hudayberdiyevoraz@gmail.com>
2024-03-29 02:41:16 +0100

    car controller and @RequestParam

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,17 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, Model model) {
+
+        model.addAttribute("make", make);
+        return "/car/carInfo";
+    }
+}
