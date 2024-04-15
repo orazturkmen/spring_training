56963b893be7f523d6d391653cce654175b07239
oraz <hudayberdiyevoraz@gmail.com>
2024-04-15 02:14:12 +0200

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -3,7 +3,12 @@ package com.cydeo.controller;
 import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -12,7 +17,21 @@ public class MentorController {
     @RequestMapping("/register")
     public String register(Model model){
 
+        List<String> batchList = Arrays.asList("Select Batch","JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchList);
         model.addAttribute("mentor", new Mentor());
         return "/mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(){
+
+        return "/mentor/mentor-confirmation";
+    }
+
+    @GetMapping("/confirm")
+    public String submitForm2(){
+
+        return "/mentor/mentor-confirmation";
     }
 }
