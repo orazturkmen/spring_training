a634aef4f4ca6e1dfd70ba3d52dc9bc077adb174
oraz <hudayberdiyevoraz@gmail.com>
2024-04-21 22:42:56 +0200

    Employee Lab

---
+++ employee-registration-project/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,26 @@
+package com.cydeo.bootstrap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+    public static List<String> getAllStates(){
+        return Arrays.asList(
+                "Alaska",
+                "Alabama",
+                "Arkansas",
+                "Arizona",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Iowa",
+                "Idaho",
+                "Illinois",
+                "New York"
+        );
+    }
+}
---
+++ employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @RequestMapping("/register")
+    public String createEmployee(Model model){
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+        return "employee/employee-create";
+    }
+}
