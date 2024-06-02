e11d259919d0e71230e12cf20d2d5c1c9381714e
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-06-03 00:41:14 +0200

    Spring 14 Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,26 @@
+package com.cydeo;
+
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryDemo implements CommandLineRunner {
+
+    public final RegionRepository regionRepository;
+
+    public QueryDemo(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("findByCountry" + regionRepository.findByCountry("Canada"));
+        System.out.println("findByCountryContaining" + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingDesc" + regionRepository.findByCountryContainsOrderByRegionDesc("United"));
+        System.out.println("findByCountryContaining" + regionRepository.findByCountryContainsOrderByRegion("United"));
+        System.out.println("findTopByCountry" + regionRepository.findTopByCountry("United States"));
+        System.out.println("findTop2ByCountry" + regionRepository.findTop2ByCountry("United States"));
+    }
+}
---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -0,0 +1,24 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RegionRepository extends JpaRepository<Region,Integer> {
+
+    //Display all regions in Canada
+    List<Region> findByCountry(String country);
+
+    //Display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
+
+    //Display all regions with country name includes 'United' in order(region)
+    List<Region> findByCountryContainsOrderByRegionDesc(String country);
+    List<Region> findByCountryContainsOrderByRegion(String country);
+
+    //Display top 2 regions in United States
+    List<Region> findTopByCountry(String country);
+    List<Region> findTop2ByCountry(String country);
+}
