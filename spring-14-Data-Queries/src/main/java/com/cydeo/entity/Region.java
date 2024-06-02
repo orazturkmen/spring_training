8872b8c8ff552336dcb60067ad2a591df7c21c1d
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-06-03 00:02:06 +0200

    Spring 14 Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
@@ -0,0 +1,15 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "regions")
+@NoArgsConstructor
+@Data
+public class Region extends BaseEntity{
+    private String region;
+    private String country;
+}
