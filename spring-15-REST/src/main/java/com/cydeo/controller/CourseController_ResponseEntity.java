2fab85c3e137375d9bad803cb3c249ce846b8320
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-06-21 01:03:03 +0200

    REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -4,9 +4,7 @@ import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,24 @@ public class CourseController_ResponseEntity {
                 .header("Version", "Cydeo.V2")
                 .header("Operation", "Get List")
                 .body(courseService.getCourses());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId){
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
+    }
+
+    @GetMapping("category/{name}")
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category){
+        return ResponseEntity
+                .ok(courseService.getCoursesByCategory(category));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation", "Create")
+                .body(courseService.createCourse(course));
     }
 }
