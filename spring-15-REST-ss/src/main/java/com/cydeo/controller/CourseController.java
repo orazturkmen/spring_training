1219b0eca0659cb30cbec09765fa895a0662e0c6
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-07-31 18:23:59 +0200

    Rest 15 ss

---
+++ spring-15-REST-ss/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourse(){
+        return courseService.getCourses();
+    }
+
+
+}
