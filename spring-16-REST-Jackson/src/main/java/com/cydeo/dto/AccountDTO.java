f2ab7f7711250c0f4fdb4049762493941056b082
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-08-01 18:49:17 +0200

    Rest 16 Jackson, @JsonBackReference,@JsonManagedReference and  other annotations

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,20 @@
 package com.cydeo.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = "address")  // ignore fields
+@JsonIgnoreProperties(ignoreUnknown = true)//if there are no fields don't match
 public class AccountDTO {
 
+    @JsonIgnore
     private String name;
     private String address;
     private String country;
@@ -14,6 +22,8 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+
+    @JsonBackReference // this field is not going to be serialized
     private UserDTO user;
 
 }
--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,17 +1,27 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.UserRole;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //don't show this field in ui
     private String password;
     private String username;
     private UserRole role;
+
+    @JsonManagedReference // this field going to be serialized
     private AccountDTO account;
 
 }
